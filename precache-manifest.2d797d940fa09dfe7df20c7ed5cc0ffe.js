caad6a23d9ee1da3d733d8382e0740682f9b5205
shivang02 <shivang.dubey1@student.amity.edu>
2020-07-10 01:42:45 +0530

    Updates

---
+++ precache-manifest.2d797d940fa09dfe7df20c7ed5cc0ffe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49cc9e6ceab7a40a1d3d5f250c48788",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "32fb72a6ab29d44b4fa2",
+    "url": "/pomodoro-clock/static/css/main.86c87aaa.chunk.css"
+  },
+  {
+    "revision": "e4febc47bf9f9c672b5d",
+    "url": "/pomodoro-clock/static/js/2.64462d57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.64462d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32fb72a6ab29d44b4fa2",
+    "url": "/pomodoro-clock/static/js/main.0e6b8e3a.chunk.js"
+  },
+  {
+    "revision": "e4e241506d5e6f2f6a21",
+    "url": "/pomodoro-clock/static/js/runtime-main.58f6ad21.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/pomodoro-clock/static/media/digital-7.1e670d88.ttf"
+  }
+]);
